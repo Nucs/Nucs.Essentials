0c5c9006fdac9d699cd6be3c2e49eeb8856a23f6
Nucs <elibelash@gmail.com>
2022-10-04 15:03:09 +0300

    Removed junk

--- src/Nucs.Essentials/SourceWaitHandle.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Threading;
-
-namespace Nucs {
-    public static class SourceWaitHandle {
-        public static AutoResetEvent ProcessingCompleteEvent = new AutoResetEvent(false);
-    }
-}

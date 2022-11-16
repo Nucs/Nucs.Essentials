a3f8931798dee49be6d178d4134bde4c5024f731
Nucs <elibelash@gmail.com>
2022-11-16 20:20:38 +0200

    Added Benchmark as .NET 7

---
+++ benchmark/Nucs.Essentials.Benchmarks/Program.cs
@@ -0,0 +1,9 @@
+using BenchmarkDotNet.Running;
+
+namespace Nucs.Essentials.Benchmarks {
+    public class Program {
+        public static void Main(string[] args) {
+            var summary = BenchmarkRunner.Run<Benchmarks>();
+        }
+    }
+}

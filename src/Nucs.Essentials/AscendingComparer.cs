ff200f8eab6edd548f89bd1387c2d11770627764
Nucs <elibelash@gmail.com>
2022-12-02 13:06:52 +0200

    Added AscendingComparer

--- src/Nucs.Essentials/AscendingComparer.cs
+++ src/Nucs.Essentials/AscendingComparer.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
 namespace Nucs {
-    public sealed class DescendingComparer<T> : IComparer<T> where T : IComparable<T> {
-        public static readonly DescendingComparer<T> Default = new();
+    public sealed class AscendingComparer<T> : IComparer<T> where T : IComparable<T> {
+        public static readonly AscendingComparer<T> Default = new();
+
         [MethodImpl(MethodImplOptions.AggressiveOptimization | MethodImplOptions.AggressiveInlining)]
         public int Compare(T x, T y) {
-            return y.CompareTo(x);
+            return x.CompareTo(y);
         }
     }
 }

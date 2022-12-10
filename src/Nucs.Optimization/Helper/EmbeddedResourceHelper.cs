9a88de1a53dbd0e8b0ee8807a72decef0a9b6378
Nucs <elibelash@gmail.com>
2022-12-10 15:48:23 +0200

    Nucs.Optimization: fixed assembly access

--- src/Nucs.Optimization/Helper/EmbeddedResourceHelper.cs
+++ src/Nucs.Optimization/Helper/EmbeddedResourceHelper.cs
@@ -1,8 +1,10 @@
+using Nucs.Optimization.Analayzer;
+
 namespace Nucs.Optimization.Helper; 
 
 public static class EmbeddedResourceHelper {
     public static string? ReadEmbeddedResource(string resourceName) {
-        var assembly = Assembly.GetExecutingAssembly();
+        var assembly = typeof(ParametersAnalyzer<>).Assembly;
         var resource = assembly.GetManifestResourceNames();
         var exactResourceName = resource.FirstOrDefault(r => r.EndsWith(resourceName));
         if (exactResourceName == null) return null;

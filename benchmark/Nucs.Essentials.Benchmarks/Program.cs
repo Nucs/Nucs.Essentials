9d6c3fe62894aeffa2944e85c154dcb930a3b309
Nucs <elibelash@gmail.com>
2023-01-06 15:44:32 +0200

    Fixed build

--- benchmark/Nucs.Essentials.Benchmarks/Program.cs
+++ benchmark/Nucs.Essentials.Benchmarks/Program.cs
@@ -3,7 +3,7 @@ using BenchmarkDotNet.Running;
 namespace Nucs.Essentials.Benchmarks {
     public class Program {
         public static void Main(string[] args) {
-            var summary = BenchmarkRunner.Run<ASCIIEncodingEncodeBenchmark>();
+            //var summary = BenchmarkRunner.Run<ASCIIEncodingEncodeBenchmark>();
         }
     }
 }
